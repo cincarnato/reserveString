15063a061ae0e4579d6ed75076c6b2d399f4527e
Cristian Incarnato <cristian.cdi@gmail.com>
2021-01-21 16:20:22 -0300

    Desafio resuelto

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const reverseString = function(str){
 
     /* YOUR CODE HERE */
+   /* let result = ""
+
+    for(let i = str.length -1; i >=0; i--)
+        result += str[i]
+
+    return result*/
+
+    return str.split("").reverse().join("")
 
 }
 
